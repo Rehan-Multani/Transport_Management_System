8ca3d1826809c115a39e21481f20b4525f5e5c3b
Muhammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2026-04-20 22:05:08 +0530

    Remove required message from password field

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true, 'Please add a password'],
         minlength: 3,
         select: false,
     },
